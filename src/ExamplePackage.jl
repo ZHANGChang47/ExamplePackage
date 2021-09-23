a8895adeb2256878874030940e43b3d99c37f719
Shu-Juan <2648492063@qq.com>
2021-09-23 19:54:36 +0800

    tijiao

---
+++ src/ExamplePackage.jl
@@ -0,0 +1,9 @@
+module ExamplePackage
+
+greet() = print("Hello World!")
+
+export greet
+
+println("你好")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Test
+
+@testset "ExamplePackage" begin
+    @test 1==1
+    @test 2==3
+end
